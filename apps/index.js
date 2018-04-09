087770bd0ef8d0f3bf229d328435dfeb513a19b0
zhaochuanchuan <zhaochuanchuan@hualala.com>
2018-04-09 15:42:09 +0800

    在project1项目中显示时间

--- apps/index.js
+++ apps/index.js
@@ -1,11 +1,16 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native';
+
 import ModalBox from '../libs/ModalBox';
+import SelectTime from '../libs/SelectTime';
 
 export default class Root extends Component{
   constructor(props) {
     super(props);
+    this.state = {
+      time: '00:00 - 00:00'
+    }
   }
   render() {
     return (
@@ -15,16 +20,23 @@ export default class Root extends Component{
             <Text style={styles.btnTxt}>弹窗1</Text>
           </View>
         </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => this.refs.selectTimeRef.show()}>
           <View style={[styles.btnWrapper, styles.center, { marginTop: 10, }]}>
             <Text style={styles.btnTxt}>日期组件</Text>
           </View>
         </TouchableWithoutFeedback>
+        <Text>{this.state.time}</Text>
 
         <ModalBox ref="modalBoxRef" color="#ed738d" />
+        <SelectTime ref="selectTimeRef" color="#ed738d" confirm={(start, end) => this.showTime(`${start}-${end}`)} />
       </View>
     )
   }
+
+  showTime = (time) => {
+    this.setState({ time })
+  }
+
 }
 const styles = StyleSheet.create({
   wrapper: {
